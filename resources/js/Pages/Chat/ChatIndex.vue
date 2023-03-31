09444c657ef8d346dbbca7b546990dabe5727cd6
Oakekywe <oakekywephyohann99@gmail.com>
2023-04-01 01:58:12 +0630

    some vue

---
+++ resources/js/Pages/Chat/ChatIndex.vue
@@ -0,0 +1,24 @@
+<template>
+  <ChatLayout>
+    <template #aside>
+    </template>
+    <div class="w-full flex text-white">
+
+    </div>
+    <template #form>
+        <section class="px-6 top-0">
+            <div class="w-full">
+                <div class="relative flex-1 flex items-center">
+                    <input type="text" class="w-full bg-slate-700 text-white rounded-lg" placeholder="Ask Laravel AI" name="">
+                </div>
+            </div>
+        </section>
+    </template>
+  </ChatLayout>
+</template>
+
+<script setup>
+import ChatLayout from '@/Layouts/ChatLayout.vue';
+  
+
+</script>
