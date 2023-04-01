495bdc045c832d98b648a5ddbb099e3a725fdcad
Oakekywe <oakekywephyohann99@gmail.com>
2023-04-02 00:25:25 +0630

    loading while sending

--- resources/js/Pages/Chat/ChatIndex.vue
+++ resources/js/Pages/Chat/ChatIndex.vue
@@ -3,7 +3,7 @@
     <template #aside>
       <ul class="p-2">
         <template v-for="message in messages" :key="message.id">
-          <li class="px-4 py-2 my-2 flex justify-between font-semibold text-slate-400 bg-slate-900 hover:bg-slate-700 rounded-lg duration-200">
+          <li class="cursor-pointer px-4 py-2 my-2 flex justify-between font-semibold text-slate-400 bg-slate-900 hover:bg-slate-700 rounded-lg duration-200">
             <Link :href="`/chat/${message.id}`"> {{message.context[0].content}}</Link>
           </li>
         </template>
@@ -25,12 +25,16 @@
             <div class="w-full">
                 <div class="relative flex-1 flex items-center">
                     <input type="text" class="w-full bg-slate-700 text-white rounded-lg" 
-                    placeholder="Ask Laravel AI" v-model="form.promt" @keyup.enter="submit">
+                    placeholder="Ask Laravel AI" v-model="form.promt" @keyup.enter="submit" :disabled="form.processing">
                     <div @click="submit" class="cursor-pointer absolute inset-y-0 right-0 flex items-center pl-3">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
+                      <svg v-if="!form.processing"
+                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
                       stroke="currentColor" class="w-6 h-6 -ml-8 text-slate-200">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                       </svg>
+                      <div class="dot-typing -ml-10" v-if="form.processing">
+
+                      </div>
                     </div>
                 </div>
             </div>
@@ -54,7 +58,51 @@ import ChatContent from '@/Components/ChatContent.vue'
   });
 
   const submit = ()=>{
-    form.post('/chat');
+    const url = props.chat ? `/chat/${props.chat?.id}` : "/chat";
+    form.post(url);
   }
 
 </script>
+<style>
+  .dot-typing{
+    position: relative;
+    left: -9999px;
+    width: 10px;
+    height: 10px;
+    border-radius: 5px;
+    background-color: #9880ff;
+    color: #9880ff;
+    box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff, 10014px 0 0 0 #9880ff;
+    animation: dot-typing 1.5s infinite linear;
+  }
+  @keyframes dot-typing {
+    0% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff,
+            10014px 0 0 0 #9880ff;
+    }
+    16.667% {
+        box-shadow: 9984px -10px 0 0 #9880ff, 9999px 0 0 0 #9880ff,
+            10014px 0 0 0 #9880ff;
+    }
+    33.333% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff,
+            10014px 0 0 0 #9880ff;
+    }
+    50% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px -10px 0 0 #9880ff,
+            10014px 0 0 0 #9880ff;
+    }
+    66.667% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff,
+            10014px 0 0 0 #9880ff;
+    }
+    83.333% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff,
+            10014px -10px 0 0 #9880ff;
+    }
+    100% {
+        box-shadow: 9984px 0 0 0 #9880ff, 9999px 0 0 0 #9880ff,
+            10014px 0 0 0 #9880ff;
+    }
+}
+</style>
